9077172ad4556bdf14563fe22d4c242f374b87a9
fatimasny <sinayi.fatima@gmail.com>
2024-03-16 03:26:59 -0700

    check second page plz

--- src/Pages/AllRestaurants/Content/MainSide/SidebarAllRes/ToggleFilterButtons/ToggleFilterButtonsItem/ToggleFilterButtonsItem.jsx
+++ src/Pages/AllRestaurants/Content/MainSide/SidebarAllRes/ToggleFilterButtons/ToggleFilterButtonsItem/ToggleFilterButtonsItem.jsx
@@ -3,7 +3,12 @@ export default function ToggleFilterButtonsItem({ title }) {
   return (
     <div className="toggle-filter-buttons-item-component">
       <p>{title}</p>
-      <div className="toggle-btn">btn</div>
+      <div className="toggle-btn">
+        <label className="switch">
+          <input type="checkbox" />
+          <span className="slider round"></span>
+        </label>
+      </div>
     </div>
   );
 }
